bfbc6d162052a6c5c8d4fe4d80f070003cc64ad2
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-05-14 01:25:06 +0200

    feat: adding new player to db from website works

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,4 +1,10 @@
 from flask import Flask
+from pymongo import MongoClient
+
+#clent = MongoClient('127.0.0.1', 27017, username='root', password='root')
+client = MongoClient('mongodb://root:root@pkims-moviehub-mongo-container-1')
+db = client['user-data']
+collection = db['users']
 
 def create_app():
     app = Flask(__name__)
--- src/website/auth.py
+++ src/website/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, request, flash
+from . import collection
 
 auth = Blueprint('auth', __name__)
 
@@ -13,20 +14,26 @@ def logout():
 
 @auth.route("/sign_up", methods=['GET', 'POST'])
 def sign_up():
-    if request.method == 'POST':
-        email = request.form.get('email')
-        first_name = request.form.get('firstName')
-        password1 = request.form.get('password1')
-        password2 = request.form.get('password2')
+    try:
+        if request.method == 'POST':
+            email = request.form.get('email')
+            first_name = request.form.get('firstName')
+            password1 = request.form.get('password1')
+            password2 = request.form.get('password2')
 
-        if len(email) < 4:
-            flash('Email is too short, must be at least 4 characters', category='alert-danger')
-        elif len(first_name) < 2:
-            flash('First name is too short, must be at least 2 characters', category='alert-danger')
-        elif password1 != password2:
-            flash('Difference between passwords', category='alert-danger')
-        elif len(password1) < 7:
-            flash('Password is too short, must be at least 7 characters', category='alert-danger')
-        else:
-            flash('Account created!', category='alert-success')
-    return render_template("sign_up.html")
+            if len(email) < 4:
+                flash('Email is too short, must be at least 4 characters', category='alert-danger')
+            elif len(first_name) < 2:
+                flash('First name is too short, must be at least 2 characters', category='alert-danger')
+            elif password1 != password2:
+                flash('Difference between passwords', category='alert-danger')
+            elif len(password1) < 7:
+                flash('Password is too short, must be at least 7 characters', category='alert-danger')
+            else:
+                res = collection.insert_one({'email': email, 'firstName': first_name, 'password': password1})
+                print(res)
+                flash('Account created!', category='alert-success')
+        return render_template("sign_up.html", test='> ' + str(collection.users.find()))
+    except Exception as e:
+        print(e)
+        return render_template("sign_up.html", test=e)
