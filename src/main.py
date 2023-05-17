1f239922685d43b9979684ad4a972e5e761f7e14
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-05-17 11:30:12 +0200

    feat: added imdb API KEY

--- src/main.py
+++ src/main.py
@@ -1,5 +1,9 @@
 from website import create_app
 
+# example call: http://www.omdbapi.com/?apikey=8fa3bc0c&t=Spider-Man&y=2002
+API_WEBSITE = 'http://www.omdbapi.com'
+API_KEY = '8fa3bc0c'
+
 app = create_app()
 
 if __name__ == '__main__':
