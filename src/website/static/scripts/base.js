90a50d79a20c410038a92b915d928cfa689747b0
Mateusz Pruss <235953@edu.p.lodz.pl>
2023-06-20 09:27:38 +0200

    feat: show img and title depending on user input from API

--- src/website/static/scripts/base.js
+++ src/website/static/scripts/base.js
@@ -1,4 +1,25 @@
 function getValueFromMovieSearch(){
     var movieTitle = document.getElementById("movieSearch").value;
     console.log(movieTitle);
+
+    const data = { dataFromJS: movieTitle };
+
+    fetch('/receive_data', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(responseData => {
+      // Handle the response from Flask server
+      console.log(responseData);
+      document.getElementById("ls-movie-title").innerHTML = responseData["Title"]
+      document.getElementById("poster").innerHTML = `<img src="${responseData['Poster']}" alt="movie title"/>`
+    })
+    .catch(error => {
+      // Handle any errors that occur during the request
+      console.error('Error:', error);
+    });
 }
--- src/website/views.py
+++ src/website/views.py
@@ -1,7 +1,12 @@
+import json
+from urllib.request import urlopen
+
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_user, login_required, logout_user, current_user
 
 views = Blueprint('views', __name__)
+
+API_LINK = 'http://www.omdbapi.com/?apikey=8fa3bc0c&t='
 
 @views.route('/')
 @views.route('/home')
@@ -12,4 +17,14 @@ def home():
 @views.route('/movie')
 @login_required
 def movie():
-    return render_template("movie.html", user=current_user)
+    return render_template("movie.html", user=current_user, movie_title="placeholder")
+
+@views.route('/receive_data', methods=["POST"])
+def receive_data():
+    data = request.get_json()  # Get the JSON data sent from the client
+    received_data = data.get('dataFromJS')  # Access the specific data field
+    print(received_data)
+
+    response = urlopen(API_LINK + received_data)
+    data_json = json.loads(response.read())
+    return data_json
